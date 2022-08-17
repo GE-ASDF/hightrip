df90acdfa5a586a18b84c9e4bd018810a9fb4e80
Anderson Souza <andersonsouza007@live.com>
2022-08-17 10:59:43 -0300

    Inserção de animações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,16 @@
 const vm = new Vue({
     el:"#app",
     data:{
-
+        menuAtivo: false,
     },
     methods:{
-        openMenu(e){
-            let $nav = document.querySelector(".nav");
-            $nav.classList.toggle("opened");
+        openMenu(){
+            this.menuAtivo = !this.menuAtivo
+        },
+        fecharMenu({target, currentTarget}){
+            if(target === currentTarget){
+                this.menuAtivo = !this.menuAtivo
+            }
         }
     }
 })
